c9ee4a7b6be0f05e6966b7cb7a83640bca1bbe75
Sam Thomas <samthomas@davidson.edu>
2020-11-29 13:23:40 -0500

    Error in income tests, will only run MNIST tests for now

--- tests.sh
+++ tests.sh
@@ -1,27 +1,27 @@
-python3 income_tester.py 5 1 100 50 50 0
-python3 income_tester.py 5 1 100 50 50 1
-python3 income_tester.py 5 1 100 50 50 2
-python3 income_tester.py 5 1 100 50 50 3
-python3 income_tester.py 5 2 100 50 50 0
-python3 income_tester.py 5 2 100 50 50 1
-python3 income_tester.py 5 2 100 50 50 2
-python3 income_tester.py 5 2 100 50 50 3
-python3 income_tester.py 5 4 100 50 50 0
-python3 income_tester.py 5 4 100 50 50 1
-python3 income_tester.py 5 4 100 50 50 2
-python3 income_tester.py 5 4 100 50 50 3
-python3 income_tester.py 5 8 100 50 50 0
-python3 income_tester.py 5 8 100 50 50 1
-python3 income_tester.py 5 8 100 50 50 2
-python3 income_tester.py 5 8 100 50 50 3
-python3 income_tester.py 5 16 100 50 50 0
-python3 income_tester.py 5 16 100 50 50 1
-python3 income_tester.py 5 16 100 50 50 2
-python3 income_tester.py 5 16 100 50 50 3
-python3 income_tester.py 5 32 100 50 50 0
-python3 income_tester.py 5 32 100 50 50 1
-python3 income_tester.py 5 32 100 50 50 2
-python3 income_tester.py 5 32 100 50 50 3
+# python3 income_tester.py 5 1 100 50 50 0
+# python3 income_tester.py 5 1 100 50 50 1
+# python3 income_tester.py 5 1 100 50 50 2
+# python3 income_tester.py 5 1 100 50 50 3
+# python3 income_tester.py 5 2 100 50 50 0
+# python3 income_tester.py 5 2 100 50 50 1
+# python3 income_tester.py 5 2 100 50 50 2
+# python3 income_tester.py 5 2 100 50 50 3
+# python3 income_tester.py 5 4 100 50 50 0
+# python3 income_tester.py 5 4 100 50 50 1
+# python3 income_tester.py 5 4 100 50 50 2
+# python3 income_tester.py 5 4 100 50 50 3
+# python3 income_tester.py 5 8 100 50 50 0
+# python3 income_tester.py 5 8 100 50 50 1
+# python3 income_tester.py 5 8 100 50 50 2
+# python3 income_tester.py 5 8 100 50 50 3
+# python3 income_tester.py 5 16 100 50 50 0
+# python3 income_tester.py 5 16 100 50 50 1
+# python3 income_tester.py 5 16 100 50 50 2
+# python3 income_tester.py 5 16 100 50 50 3
+# python3 income_tester.py 5 32 100 50 50 0
+# python3 income_tester.py 5 32 100 50 50 1
+# python3 income_tester.py 5 32 100 50 50 2
+# python3 income_tester.py 5 32 100 50 50 3
 python3 tester.py 5 1 100 50 50 0
 python3 tester.py 5 1 100 50 50 1
 python3 tester.py 5 1 100 50 50 2
